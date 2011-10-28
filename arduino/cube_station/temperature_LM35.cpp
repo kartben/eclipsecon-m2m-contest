e03329d08747769670b66540e430d0e06aa30a05
Benjamin Cabé <BCabe@sierrawireless.com>
2011-10-28 17:05:13 +0200

    No more offset needed!!! This was just a problem of power supply ...

--- arduino/cube_station/temperature_LM35.cpp
+++ arduino/cube_station/temperature_LM35.cpp
@@ -4,10 +4,6 @@
 float getTemperature(int pin) {
   float C = ( analogRead(pin) *0.0048828125 * 100. ) - 273. ;
 
-  // there seems to be an offset of about 25C.
-  // Would probably be better to really calibrate the sensor with its dedicated pin though..
-  C = C - 25.0 ; 
-
   return C ;
 }
 
